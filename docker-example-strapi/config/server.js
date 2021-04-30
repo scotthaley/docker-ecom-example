a1a2888983eb58c31a5238f6f09e75c5af4b672d
Scott Haley <scott.haley@am.jll.com>
2021-04-30 14:56:15 -0500

    Add strapi and postgres db to docker-compose

---
+++ docker-example-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbc26d4178493b2fd62464fc6dd1568d'),
+    },
+  },
+});
---
+++ docker-example-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cc081eb6-9f16-4347-b0fc-8498a38ceed8'
+};
